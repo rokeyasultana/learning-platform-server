c13ce850042366aa9c54bb2609f7593f9883349c
jakiasultana2022 <rokeyasbd@gmail.com>
2022-10-26 21:01:56 +0600

    vercel json

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const port =process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const courses = require('./data/courses.json')
+const courses = require('./data/courses.json');
+
 
 app.get('/courses',(req,res)=>{
-res.send(courses)
+res.send(courses);
+});
 
-})
 
+
+app.get('/courses/:id', async(req, res) =>{
+  const id = req.params.id;
+  const course =  courses.find(c=> c.id ===id);
+  res.send(course);
+});
 
 
 app.get('/', (req, res) => {
